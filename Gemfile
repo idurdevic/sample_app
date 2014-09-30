b029bd9dbd688eb795a39c4c20599f927ddb0d59
Ivana <ivana.durdevic@gmail.com>
2014-09-30 19:57:31 +0000

    Finish layout and routes

--- Gemfile
+++ Gemfile
@@ -7,6 +7,7 @@ gem 'coffee-rails', '4.0.1'
 gem 'jquery-rails', '3.1.1'
 gem 'turbolinks',   '2.2.2'
 gem 'jbuilder',     '2.1.3'
+gem 'bootstrap-sass',       '3.2.0.0'
 gem 'sdoc',         '0.4.0', group: :doc
 
 group :test do
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -23,4 +23,11 @@ class StaticPagesControllerTest < ActionController::TestCase
     assert_response :success
     assert_select "title", "#{@base_title} | About"
   end
+  
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "#{@base_title} | Contact"
+  end
+
 end
